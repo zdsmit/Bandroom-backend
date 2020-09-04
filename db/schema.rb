61912d7a427dbd0b6d6bb1c84395a53ef81111e9
zdsmi <zdsmith001@gmail.com>
2020-09-04 12:05:17 -0500

    switched db from sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_08_28_194457) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "stores", force: :cascade do |t|
     t.string "name"
     t.string "location"
